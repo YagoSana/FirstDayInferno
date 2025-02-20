ca1806ba15d464395d5d4317471c1411004fc49d
YagoSana <yagosana@ucm.es>
2025-02-20 20:32:26 +0100

    colisiones de troncos invisibles

--- src/level.js
+++ src/level.js
@@ -41,12 +41,33 @@ export default class Level extends Phaser.Scene {
     var layer6 = map.createLayer('arboles', [tileset1, tileset2, tileset3, tileset4, tileset5], 0, 0);
     var layer7 = map.createLayer('sombrasArboles', [tileset1, tileset2, tileset3, tileset4, tileset5], 0, 0);
 
+    layer6.setDepth(10);
     layer5.setCollisionByExclusion([-1], true);
 
+    // **Crear un grupo de colisiones invisibles**
+    this.troncos = this.physics.add.staticGroup();
+
+    let troncosLayer = map.getObjectLayer('colisionesObj');
+    console.log("Capa de troncos:", troncosLayer);
+
+    troncosLayer.objects.forEach(obj => {
+        // Crear un objeto invisible con colisión
+        let tronco = this.add.rectangle(
+            obj.x + obj.width / 2,
+            obj.y - obj.height / 2 + 20,
+            obj.width,
+            obj.height,
+            0x000000,
+            0 // Transparente
+        );
+        // Agregar físicas
+        this.physics.add.existing(tronco, true);
+        this.troncos.add(tronco);
+    });
     
-    //let arbustos = map.createFromObjects('arbustos', 'arbustos', { key: 'arbustos' });
-    //let piedras = map.createFromObjects('piedras', 'piedras', { key: 'piedras' });
-    //let sombraspiedras = map.createFromObjects('sombraspiedras', 'sombraspiedras', { key: 'sombraspiedras' });
+
+    
+
     
     // Crear un grupo de físicas para los objetos
     //this.obstaculos = this.physics.add.staticGroup();
@@ -75,6 +96,7 @@ export default class Level extends Phaser.Scene {
     this.physics.add.collider(this.player, layer5);
     // Añadir colisión con el jugador
     this.physics.add.collider(this.player, this.obstaculos);
+    this.physics.add.collider(this.player, this.troncos);
     new Item(this, 100, 100);
   }
 
