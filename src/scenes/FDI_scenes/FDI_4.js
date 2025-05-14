aa772e96525e9ef248366a733f4d201aef0c8705
samcarri03 <samuelcarrillo2003@gmail.com>
2025-05-14 22:26:57 +0200

    Mejoras visuales FDI

--- src/scenes/FDI_scenes/FDI_4.js
+++ src/scenes/FDI_scenes/FDI_4.js
@@ -18,12 +18,13 @@ export default class FDI_4 extends SalaBase {
         const tileset2 = map.addTilesetImage('Room_Builder_free_16x16', 'Muebles');
         const tileset3 = map.addTilesetImage('tileset_nuevo', 'Decorado');
 
-        const layer1 = map.createLayer('suelo', [tileset1, tileset2], 0, 0);
-        const layer2 = map.createLayer('pared', [tileset1, tileset2], 0, 0);
-        const layer5 = map.createLayer('sin colisiones', [tileset1, tileset2], 0, 0);
-        const layer3 = map.createLayer('objetos', [tileset1, tileset2], 0, 0);
-        const layer4 = map.createLayer('objetos2', [tileset1, tileset2], 0, 0);
-        const layer6 = map.createLayer('techo', [tileset1, tileset2], 0, 0);
+        const layer1 = map.createLayer('suelo', [tileset1, tileset2, tileset3], 0, 0);
+        const layer2 = map.createLayer('pared', [tileset1, tileset2, tileset3], 0, 0);
+      
+        const layer3 = map.createLayer('objetos', [tileset1, tileset2, tileset3], 0, 0);
+        const layer4 = map.createLayer('objetos2', [tileset1, tileset2, tileset3], 0, 0);
+          const layer5 = map.createLayer('sin colisiones', [tileset1, tileset2, tileset3], 0, 0);
+        const layer6 = map.createLayer('techo', [tileset1, tileset2, tileset3], 0, 0);
         const layer7 = map.createLayer('decoracion', [tileset1, tileset2, tileset3], 0, 0);
 
         layer2.setCollisionByExclusion([-1], true);
